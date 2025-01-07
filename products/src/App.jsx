67099273d9b76548717740dbf8b0e4b40a8ffb45
prafulpawar <prafuldukhi@gmail.com>
2025-01-07 21:20:13 +0530

    "Done"

--- products/src/App.jsx
+++ products/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import store from './store/store'
-import { Provider } from 'react-redux'
+
 function App() {
   return (
      <Provider store={store}>
--- reduxtoolkitcu/src/App.jsx
+++ reduxtoolkitcu/src/App.jsx
@@ -1,24 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
 import React from 'react'
 import Two from './components/Two'
-import {asyncgetproducts} from  './Action/productAction'
+import { asyncgetproducts } from './Action/productAction'
 import { useEffect } from "react";
 function App() {
-   
- 
+
+
 
   const dispatch = useDispatch();
   const { products } = useSelector((state) => state.products);
-   console.log(products)
+  console.log(products)
   useEffect(() => {
-      products.length === 0 && dispatch(asyncgetproducts());
+    products.length === 0 && dispatch(asyncgetproducts());
   }, []);
   return (
     <div>
 
-      <Two/>
+      <Two />
 
-    </div>  
+    </div>
   )
 }
 
