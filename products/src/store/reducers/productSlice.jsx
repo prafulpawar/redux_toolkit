166ad94d5e075965daa1b5b21dd6682795e651e9
prafulpawar <prafuldukhi@gmail.com>
2025-01-07 21:15:53 +0530

    "Done"

--- products/src/store/reducers/productSlice.jsx
+++ products/src/store/reducers/productSlice.jsx
@@ -4,3 +4,26 @@ const initialState = {
      
     ],
   };
+
+  //Producsts Slice 
+// Mujhe Data De DO Aur Naam De Do
+const productSlice = createSlice({
+    name: "products",
+    initialState,
+      reducers:{
+          getData:(state,action)=>{
+            state.products = action.payload;
+          },
+          add:(state,action)=>{
+            state.products.push(action.payload)
+          },
+         remove:(state)=>{
+             state.products.pop()
+         }
+      }
+    
+  });
+  
+  export default productSlice.reducer;
+  export const {add,remove,getData} = productSlice.actions
+  
